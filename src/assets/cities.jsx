7d7282d5f371c1e3dc722c41b5eea497e90f7695
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-12-18 21:20:34 +0400

    Add placeholder data

---
+++ src/assets/cities.jsx
@@ -0,0 +1,14 @@
+export const cities = [
+    {
+        city: 'Moscow',
+        local: 'Москва'
+    },
+    {
+        city: 'Samara',
+        local: 'Самара'
+    },
+    {
+        city: 'Warsaw',
+        local: 'Варшава'
+    }
+]
