e8b0493db1151a34f698d3601efbd8d9ac7ea1cd
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-12-19 18:11:23 +0400

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,40 @@ import pressure from './assets/pressure.png';
 import humidity from './assets/humidity.png';
 import wind from './assets/wind.png';
 
+function WeatherCard({ weather }) {
+  return (
+    <div className='grid grid-rows-[auto,auto,auto]'>
+      <div className='py-2 border-2 rounded-t-xl flex flex-col items-center'>
+        <p className='text-white text-5xl font-bold font-mono'>{weather.city.name}</p>
+        <p className='text-white text-xl font-mono'>{weather.list[0].weather[0].description}</p>
+      </div>
+      <div>
+        <p className='mt-2 border-l-2 border-r-2 text-white text-center text-5xl font-bold'>{(weather.list[0].main.temp - 273.15).toFixed()}°C</p>
+        <p className='mb-2 border-l-2 border-r-2 text-white text-center'>По ощущениям: {(weather.list[0].main.feels_like - 273.15).toFixed()}°C</p>
+      </div>
+      <div className='border-t-2 grid grid-cols-2'>
+        <div className='pl-2 border-2 border-t-0 rounded-bl-xl text-white font-medium flex flex-col justify-center'>
+          <div className='flex items-center gap-1'>
+            <img className='w-7 h-7' src={pressure}/>
+            <p>{weather.list[0].main.pressure}</p>
+          </div>
+          <div className='flex items-center gap-1'>
+            <img className='w-7 h-7' src={humidity}/>
+            <p>{weather.list[0].main.humidity}%</p>
+          </div>
+        </div>
+        <div className='p-2 border-2 border-t-0 rounded-br-xl text-white font-medium flex gap-1'>
+          <img className='w-7 h-7' src={wind}/>
+          <div>
+            <p>{weather.list[0].wind.speed} м/с</p>
+            <p>{weather.list[0].wind.deg}°</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   const [weather, setWeather] = useState(null)
   const [location, setLocation] = useState({ error: 'Разрешить использовать своё местоположение', disabled: false })
@@ -71,35 +105,7 @@ function App() {
               <button className='p-2 border-2 border-indigo-500 rounded text-white font-medium transition ease-in-out hover:bg-indigo-600' onClick={() => getWeather()}>Посмотреть погоду</button>
             </div>
             :
-            <div className='grid grid-rows-[auto,auto,auto]'>
-              <div className='py-2 border-2 rounded-t-xl flex flex-col items-center'>
-                <p className='text-white text-5xl font-bold font-mono'>{weather.city.name}</p>
-                <p className='text-white text-xl font-mono'>{weather.list[0].weather[0].description}</p>
-              </div>
-              <div>
-                <p className='mt-2 border-l-2 border-r-2 text-white text-center text-5xl font-bold'>{(weather.list[0].main.temp - 273.15).toFixed()}°C</p>
-                <p className='mb-2 border-l-2 border-r-2 text-white text-center'>По ощущениям: {(weather.list[0].main.feels_like - 273.15).toFixed()}°C</p>
-              </div>
-              <div className='border-t-2 grid grid-cols-2'>
-                <div className='pl-2 border-2 border-t-0 rounded-bl-xl text-white font-medium flex flex-col justify-center'>
-                  <div className='flex items-center gap-1'>
-                    <img className='w-7 h-7' src={pressure}/>
-                    <p>{weather.list[0].main.pressure}</p>
-                  </div>
-                  <div className='flex items-center gap-1'>
-                    <img className='w-7 h-7' src={humidity}/>
-                    <p>{weather.list[0].main.humidity}%</p>
-                  </div>
-                </div>
-                <div className='p-2 border-2 border-t-0 rounded-br-xl text-white font-medium flex gap-1'>
-                  <img className='w-7 h-7' src={wind}/>
-                  <div>
-                    <p>{weather.list[0].wind.speed} м/с</p>
-                    <p>{weather.list[0].wind.deg}°</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <WeatherCard weather={weather}/>
             }
           </div>
         </div>
