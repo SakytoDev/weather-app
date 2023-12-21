cd42409b53b9755f7ba20c105434a997c5593ca6
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-12-21 17:12:24 +0400

    Add card hover animation

--- src/components/forecastcard.jsx
+++ src/components/forecastcard.jsx
@@ -4,7 +4,7 @@ import { DateTime } from 'luxon';
 
 export default function ForecastCard({ weather, index, setWeatherIndex, delay }) {
   return (
-    <motion.button initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 20 }} transition={{ ease: 'easeInOut', delay: delay }} className='px-5 py-4 bg-zinc-700 border-2 rounded-xl text-xl text-white font-bold' onClick={() => setWeatherIndex(index)}>
+    <motion.button initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0, transition: { delay: delay } }} exit={{ opacity: 0, y: 20 }} whileHover={{ scale: 1.05, transition: { duration: 0.1 } }} transition={{ ease: 'easeInOut' }} className='px-5 py-4 bg-zinc-700 border-2 rounded-xl text-xl text-white font-bold' onClick={() => setWeatherIndex(index)}>
       <div className='grid grid-rows-2 items-stretch'>
         <div className='flex'>
           <p className='text-white text-2xl font-bold'>{(weather.temp.day).toFixed()}°C</p>
