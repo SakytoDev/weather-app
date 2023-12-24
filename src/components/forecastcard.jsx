92e3888f1233c38c61748e2f4e13a820e4cce794
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-12-24 16:36:44 +0400

    Display day and night temperature

--- src/components/forecastcard.jsx
+++ src/components/forecastcard.jsx
@@ -7,7 +7,10 @@ export default function ForecastCard({ weather, index, setWeatherIndex, delay, c
     <motion.button initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0, transition: { delay: delay } }} exit={{ opacity: 0, y: 20 }} transition={{ ease: 'easeInOut' }} className={`px-5 py-4 bg-gradient-to-br from-zinc-600 to-zinc-900 border-2 rounded-xl text-xl text-white font-bold ${className}`} onClick={() => setWeatherIndex(index)}>
       <div className='grid grid-rows-2 items-stretch'>
         <div className='flex'>
-          <p className='text-white text-2xl font-bold'>{(weather.temp.day).toFixed()}°C</p>
+          <div className='text-white text-left text-lg font-medium'>
+            <p>Днём: {(weather.temp.day).toFixed()}°C</p>
+            <p>Ночью: {(weather.temp.night).toFixed()}°C</p>
+          </div>
           <div className='font-medium font-mono text-right ml-auto'>
             <p className='text-white text-2xl'>{DateTime.fromSeconds(weather.dt).setLocale('ru').toFormat('dd LLL')}</p>
             <p className='text-white text-base'>{DateTime.fromSeconds(weather.dt).toRelativeCalendar()}</p>
