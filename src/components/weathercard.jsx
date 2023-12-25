eb727036a985442a9882e867486ee5e5036d0c7e
Konstantin Zasherinsky <procsssrus@mail.ru>
2023-12-25 18:20:23 +0400

    Ability to check any time of day

--- src/components/weathercard.jsx
+++ src/components/weathercard.jsx
@@ -1,15 +1,55 @@
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { DateTime } from 'luxon';
 
-import pressure from '../assets/pressure.png';
-import humidity from '../assets/humidity.png';
-import wind from '../assets/wind.png';
-import arrow from '../assets/windArrow.png';
+import pressure from '../assets/weathercard/pressure.png';
+import humidity from '../assets/weathercard/humidity.png';
+import wind from '../assets/weathercard/wind.png';
+import arrow from '../assets/weathercard/windArrow.png';
+
+import morn from '../assets/weathercard/morn.png';
+import day from '../assets/weathercard/day.png';
+import eve from '../assets/weathercard/eve.png';
+import night from '../assets/weathercard/night.png';
 
 export default function WeatherCard({ weather, index }) {
+  const [typeIndex, setTypeIndex] = useState(0)
+  const dayTypes = [ 
+    { 
+      icon: morn,
+      local: 'Утром:',
+      temp: weather.daily[index].temp.morn,
+      feels_like: weather.daily[index].feels_like.morn
+    },
+    { 
+      icon: day,
+      local: 'Днём:',
+      temp: weather.daily[index].temp.day,
+      feels_like: weather.daily[index].feels_like.day
+    },
+    { 
+      icon: eve,
+      local: 'Вечером:',
+      temp: weather.daily[index].temp.eve,
+      feels_like: weather.daily[index].feels_like.eve
+    },
+    { 
+      icon: night,
+      local: 'Ночью:',
+      temp: weather.daily[index].temp.night,
+      feels_like: weather.daily[index].feels_like.night
+    }
+  ]
+
   function degreesToText(degree) {
     const degArr = ['С', 'ССВ', 'СВ', 'ВСВ', 'В', 'ВЮВ', 'ЮВ', 'ЮЮВ', 'Ю', 'ЮЮЗ', 'ЮЗ', 'ЗЮЗ', 'З', 'ЗСЗ', 'СЗ', 'ССЗ']
 
     return degArr[((degree / 22.5) - 0.5).toFixed()]
+  }
+
+  function changeDayType() {
+    if (typeIndex == dayTypes.length - 1) setTypeIndex(0)
+    else setTypeIndex(prev => prev + 1)
   }
 
   return (
@@ -24,9 +64,35 @@ export default function WeatherCard({ weather, index }) {
           </div>
         </div>
       </div>
-      <div className='py-2 border-l-2 border-r-2 text-center'>
-        <p className='text-5xl font-bold'>{(weather.daily[index].temp.day).toFixed()}°C</p>
-        <p>По ощущениям: {(weather.daily[index].feels_like.day).toFixed()}°C</p>
+      <div className='p-2 border-l-2 border-r-2 flex'>
+        <AnimatePresence mode='wait'>
+          { dayTypes.map((item, index) => {
+            if (index == typeIndex) {
+              return (
+                <motion.div key={index} initial={{ x: 10, opacity: 0}} animate={{ x: 0, opacity: 1 }} exit={{ x: -10, opacity: 0 }} transition={{ ease: 'easeInOut', duration: 0.2 }} className='ml-2'>
+                  <p className='text-xl font-bold'>{dayTypes[typeIndex].local} {(dayTypes[typeIndex].temp).toFixed()}°C</p>
+                  <p>По ощущениям: {(dayTypes[typeIndex].feels_like).toFixed()}°C</p>
+                </motion.div>
+              )
+            }
+          })}
+        </AnimatePresence>
+        <button className='px-2 border-2 border-indigo-500 rounded-xl ml-auto transition ease-in-out hover:bg-indigo-600' onClick={() => changeDayType()}>
+          <AnimatePresence mode='wait'>
+            { dayTypes.map((item, index) => {
+              if (index == typeIndex) {
+                return <motion.img
+                  key={index}
+                  initial={{ y: 5, opacity: 0 }} 
+                  animate={{ y: 0, opacity: 1 }} 
+                  exit={{ y: -5, opacity: 0 }} 
+                  transition={{ ease: 'easeInOut', duration: 0.2 }}
+                  className='w-8 h-8' 
+                  src={dayTypes[typeIndex].icon}/>
+              }
+            })}
+          </AnimatePresence>
+        </button>
       </div>
       <div className='border-t-2 grid grid-cols-2'>
         <div className='pl-2 border-2 border-t-0 rounded-bl-xl font-medium flex flex-col justify-center'>
